20c0029b0eb759e49b844a6ede02efb22fc0b0c7
Denis Latyshev <d.d.latyshev@yandex.ru>
2024-01-18 17:16:58 +0300

    feat: сделать верстку страницы forgot-password

---
+++ src/app/forgot-password/page.tsx
@@ -0,0 +1,75 @@
+'use client'
+
+import { useState, useCallback } from "react"
+import { Button, EmailInput } from "@ya.praktikum/react-developer-burger-ui-components"
+import { forgotPasswordRequest } from "@/utils/api"
+import { useRouter } from "next/navigation"
+import onlyUnAuth from "@/components/onlyUnAuth/onlyUnAuth"
+import Link from "next/link"
+
+const ForgotPassword = () => {
+
+  const [email, setEmail] = useState('')
+  const [error, setError] = useState({hasError: false, errorMessage: ''})
+
+  const handleSubmit = useCallback(
+    async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      try {
+        await forgotPasswordRequest(email)
+        // navigate('/reset-password', {state:{from: location}})
+      } catch (e) {
+        setError({hasError: true, errorMessage: e as string})
+      }
+    },
+    [email]
+  )
+
+  return (
+    <main className='flex justify-center items-start sm:items-center h-[calc(100vh-64px)] xl:h-[calc(100vh-102px)]'>
+      <form 
+        className='flex flex-col items-center'
+        onSubmit={handleSubmit}
+      >
+        <p className="font-jet text-center text-[28px] sm:text-2xl mt-[20px] sm:mt-[0px]">
+          Восстановление пароля
+        </p>
+        <EmailInput 
+          placeholder={'Указжите e-mail'} 
+          extraClass="mt-6 mb-6 !w-[304px] flex sm:!w-[480px] sm:block" 
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          name = {'email'}
+        />
+        <Button 
+          htmlType="submit" 
+          type="primary" 
+          size="medium" 
+        >
+          Восстановить
+        </Button>
+        { 
+          error
+          &&
+          (
+            <p className="text text_type_main-default text_color_inactive mt-4">
+              {error.errorMessage}
+            </p>
+          ) 
+        }
+        <div className='flex flex-col items-center gap-2 mt-[40px] lg:mt-[80px] lg:flex-row'>
+          <p className="font-jet text-sm lg:text-base text_color_inactive">
+            Вспомнили пароль?
+          </p>
+          <Link href="/register">
+            <p className='font-jet text-sm lg:text-base text-[#4C4CFF]'>
+              Войти
+            </p>
+          </Link>
+        </div>
+      </form>
+    </main>
+  )
+}
+
+export default onlyUnAuth(ForgotPassword)
