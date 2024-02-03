1d06831c56f2419e39b22da8395ea6cf16eb957a
Denis Latyshev <d.d.latyshev@yandex.ru>
2024-02-03 07:17:55 +0300

    feat:добавить использование vh на основе window.innerHeight в компонент app/teamplate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,9 @@ import BurgerIngredients from "@/components/burger_ingredients/burger_ingredient
 import BurgerConstructor from "@/components/burger_constructor/burger_constructor"
 
 export default function Home() {
+    
   return (
-    <main className='grid grid-cols-1 xl:pb-10 xl:grid-cols-2 xl:grid-rows-1 gap-10 xl:w-[80%] mx-auto h-[calc(100vh-64px)] xl:h-[calc(100vh-102px)]'>
+    <main className='grid grid-cols-1 xl:pb-10 xl:grid-cols-2 xl:grid-rows-1 gap-10 xl:w-[80%] mx-auto h-[var(--main-height-beforexl)] xl:h-[var(--main-height-overxl)]'>
       <BurgerIngredients/>
       <BurgerConstructor/>
     </main>
--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from "react"
+import { useEffect, useState, useCallback } from "react"
 import { useAppDispatch } from "@/redux/store"
 import { checkUserAuth } from "@/redux/userDataSlice"
 import { usePathname } from "next/navigation"
@@ -16,6 +16,39 @@ export default function Template({ children }: { children: React.ReactNode }) {
   const user = useAppSelector((store) => store.userData.user)
   const [isAuthChecked, setIsAuthChecked] = useState(false)
 
+  // внедрен самостоятельный расчет vh на основе window.innerHeight из-за его неправильного отображения в мобильных браузерах
+  useEffect(
+    () => {
+      let root =  document.documentElement
+      if(root) {
+      root.style.setProperty('--main-height-overxl', `${window.innerHeight - 102}px`);
+      root.style.setProperty('--main-height-beforexl', `${window.innerHeight - 64}px`);
+      }
+    },
+    []
+  )
+  
+  useEffect(
+    () => {
+      
+      const handleResize = () => {
+        let root =  document.documentElement
+        if(root) {
+        root.style.setProperty('--main-height-overxl', `${window.innerHeight - 102}px`);
+        root.style.setProperty('--main-height-beforexl', `${window.innerHeight - 64}px`);
+        }
+      }
+
+      window.addEventListener('resize', handleResize)
+
+      return () => {
+        window.removeEventListener('resize', handleResize)
+      }
+
+    },
+    []
+  )
+  
   useEffect(() => {
     dispatch({type: 'FETCH_INGREDIENCES'})
   }, [])
