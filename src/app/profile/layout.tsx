318461a49cbfd19e19489e65b86967712ee9b5b1
Denis Latyshev <d.d.latyshev@yandex.ru>
2024-02-03 11:17:41 +0300

    fix: исправлен баг с отображением меню навигации при попытке зайти в личный кабинет без авторизации

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,10 +1,11 @@
 'use client'
 
 import Link from "next/link"
-import { useAppDispatch } from "@/redux/store"
+import { useAppDispatch, useAppSelector } from "@/redux/store"
 import { usePathname, useRouter } from "next/navigation"
 import { logout } from "@/redux/userDataSlice"
-import { useCallback, useState } from "react"
+import { useCallback, useState, useEffect } from "react"
+import { redirect } from "next/navigation"
 
 export default function ProfileLayout({children}: {children: React.ReactNode}) {
 
@@ -24,7 +25,29 @@ export default function ProfileLayout({children}: {children: React.ReactNode}) {
     [dispatch, router]
   )
   
-  return (
+  /* Проверка авторицации пользователя */
+  const user =  useAppSelector((store) => store.userData.user)
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(
+    () => {
+      setTimeout(
+        () => {
+          setIsMounted(true)
+        },
+        1000
+      )
+    },
+    []
+  )
+  
+  if(isMounted && !user) {
+    return redirect(`/login?from=${pathname}`)
+  }
+  
+  /* если пройдена авторизация отображаем страницу личного кабинета,
+  иначе перенаправляем на страницу авторизации */
+  return isMounted ? (
     <main className="relative flex flex-col pt-4 lg:pt-10 pl-2 lg:pl-4 lg:flex-row lg:gap-[60px] xl:w-[80%] mx-auto h-[var(--main-height-beforexl)] xl:h-[var(--main-height-overxl)] overflow-hidden">
       <section className="hidden lg:flex flex-col gap-[80px] w-[26%]">
         <nav className="flex flex-col gap-10 pt-[80px]">
@@ -67,5 +90,6 @@ export default function ProfileLayout({children}: {children: React.ReactNode}) {
       </section>
       {children}
     </main>
-  )
+  ) 
+  : null
 }
